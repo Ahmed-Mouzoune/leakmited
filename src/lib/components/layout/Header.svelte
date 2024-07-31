de410ee877b4b9d11f69414170849ba41f2f9fb2
unknown <ahmed.mouzoune92@gmail.com>
2024-08-01 00:47:07 +0200

    feat: :art: Add Header component & layout

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	import logo from '$lib/images/logo-leakmited.png';
+</script>
+
+<header class="bg-white shadow-md">
+	<div class="container mx-auto flex items-center px-4 py-2">
+		<div class="flex-shrink-0">
+			<a href="/">
+				<img src={logo} alt="Logo" class="h-10" />
+			</a>
+		</div>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/components/layout/Header.svelte';
+</script>
+
+<Header />
+<slot></slot>
